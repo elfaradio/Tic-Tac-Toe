d05775719382d4e2064fe2a00cb1483af8bd3ad6
elfaradio <farhadulislam84054@gmail.com>
2025-11-20 20:18:53 +0600

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
-import "./App.css";
+import Player from './Components/Player.jsx';
 
 function App() {
-  return <></>;
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player name="Player 1" symbol="X" />
+          <Player name="Player 2" symbol="O" />
+        </ol>
+        Farhad
+      </div>
+      LOG
+    </main>
+  );
 }
 
 export default App;
